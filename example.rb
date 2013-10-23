ade09c9bdd7a82c4b04e339d46529beac0ddb9a8
Rob Heaton <rob@pingpanel.com>
2013-10-23 16:28:14 +0100

    Tidyups

--- example.rb
+++ example.rb
@@ -10,3 +10,11 @@ require './player'
 
 player1 = Poker::Player.new("Barry")
 player2 = Poker::Player.new("Gonad")
+
+game = Poker::Game.new
+
+game_dealer = Poker::GameDealer.new(game)
+game_dealer.add_player(player1)
+game_dealer.add_player(player2)
+
+game_dealer.deal_game
